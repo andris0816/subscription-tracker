7ae109db837a0d3742b90df3625eebfcc7ce0581
Bencsik András <acelbeton0101@gmail.com>
2025-11-17 11:27:25 +0100

    feat: added subscription endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubscriptionController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +23,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/subscriptions', [SubscriptionController::class, 'index'])->name('subscription.index');
+    Route::post('subscription', [SubscriptionController::class, 'store'])->name('subscription.store');
+    Route::get('subscription/{subscription}', [SubscriptionController::class, 'show'])->name('subscription.show');
+    Route::patch('subscription/{subscription}', [SubscriptionController::class, 'update'])->name('subscription.update');
 });
 
 require __DIR__.'/auth.php';
