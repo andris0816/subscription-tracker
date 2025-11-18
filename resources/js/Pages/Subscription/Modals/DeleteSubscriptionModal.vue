1a7ad7ecc16d8e545df64dc1c0a751dadc156cb6
Bencsik András <acelbeton0101@gmail.com>
2025-11-18 11:36:40 +0100

    feat: created delete modal for subscriptions

---
+++ resources/js/Pages/Subscription/Modals/DeleteSubscriptionModal.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import DangerButton from '@/Components/DangerButton.vue';
+import Modal from '@/Components/Modal.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import {ref} from "vue";
+import {Subscription} from "@/types/subscription.interface";
+import {router} from "@inertiajs/vue3";
+
+const props = defineProps<{
+    show: boolean,
+    subscription: Subscription | null;
+}>();
+
+const deleteSubscriptionModal = ref(false);
+
+const emit = defineEmits<{
+    deleted: [id: number];
+    close: [];
+}>();
+
+const deleteSubscription = () => {
+    router.delete(route('subscription.destroy', props.subscription.id), {
+        onSuccess: () => {
+            const deletedId = usePage().props.deletedId;
+            emit('deleted', deletedId);
+            emit('close');
+        }
+    });
+};
+</script>
+
+<template>
+    <section class="space-y-6">
+        <Modal :show="show" @close="$emit('close')">
+            <div class="p-6">
+                <h2
+                    class="text-lg font-medium text-gray-900"
+                >
+                    Are you sure you want to delete this subscription?
+                </h2>
+
+                <div class="mt-6 flex justify-end">
+                    <SecondaryButton @click="$emit('close')">
+                        Cancel
+                    </SecondaryButton>
+
+                    <DangerButton
+                        class="ms-3"
+                        @click="deleteSubscription"
+                    >
+                        Delete Subscription
+                    </DangerButton>
+                </div>
+            </div>
+        </Modal>
+    </section>
+</template>
