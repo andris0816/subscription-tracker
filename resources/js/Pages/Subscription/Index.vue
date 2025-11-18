a22327832f0bdb7324f64babbdd716ab772a3f53
Bencsik András <acelbeton0101@gmail.com>
2025-11-18 11:39:00 +0100

    feat: added proper create, update and delete logic into the parent view

--- resources/js/Pages/Subscription/Index.vue
+++ resources/js/Pages/Subscription/Index.vue
@@ -1,18 +1,53 @@
 <script setup lang="ts">
 import { Subscription } from '@/types/subscription.interface';
-import CreateSubscriptionModal from "@/Pages/Subscription/CreateSubscriptionModal.vue";
-import SubscriptionCard from "@/Pages/Subscription/SubscriptionCard.vue";
+import CreateSubscriptionModal from "@/Pages/Subscription/Modals/CreateSubscriptionModal.vue";
+import SubscriptionCard from "@/Pages/Subscription/Partials/SubscriptionCard.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import {Head} from "@inertiajs/vue3";
+import {ref} from "vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import DeleteSubscriptionModal from "@/Pages/Subscription/Modals/DeleteSubscriptionModal.vue";
 
 interface Props {
     subscriptions: Subscription[];
 }
 
 const props = defineProps<Props>();
+const subscriptions = ref<Subscription[]>(props.subscriptions);
+const showCreateEditModal = ref(false);
+const showDeleteModal = ref(false);
+const selectedSubscription = ref<Subscription | null>(null);
 
-const handleSubscriptionCreated = (newSubscription: Subscription) => {
-    props.subscriptions.unshift(newSubscription);
+const handleCreated = (subscription: Subscription) => {
+    subscriptions.value.unshift(subscription);
+    showCreateEditModal.value = false;
+}
+
+const handleUpdated = (subscription: Subscription) => {
+    subscriptions.value = subscriptions.value.map(s =>
+        s.id === subscription.id ? subscription : s
+    );
+    showCreateEditModal.value = false;
+}
+
+const handleDeleted = (id: number) => {
+    subscriptions.value = subscriptions.value.filter(s => s.id !== id);
+    selectedSubscription.value = null;
+}
+
+const openCreateModal = () => {
+    selectedSubscription.value = null;
+    showCreateEditModal.value = true;
+}
+
+const openEditModal = (subscription: Subscription) => {
+    selectedSubscription.value = subscription;
+    showCreateEditModal.value = true;
+}
+
+const openDeleteModal = (subscription: Subscription) => {
+    selectedSubscription.value = subscription;
+    showDeleteModal.value = true;
 }
 </script>
 
@@ -29,10 +64,31 @@ const handleSubscriptionCreated = (newSubscription: Subscription) => {
         </template>
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
             <div class="flex items-center justify-end mb-8">
-                <CreateSubscriptionModal @created="handleSubscriptionCreated" />
+                <PrimaryButton @click="openCreateModal">Create Subscription</PrimaryButton>
+
+                <CreateSubscriptionModal
+                    :show="showCreateEditModal"
+                    :subscription="selectedSubscription"
+                    @created="handleCreated"
+                    @updated="handleUpdated"
+                    @close="showCreateEditModal = false"
+                />
             </div>
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <SubscriptionCard v-for="subscription in subscriptions" :subscription="subscription" />
+                <SubscriptionCard
+                    v-for="subscription in subscriptions"
+                    :key="subscription.id"
+                    :subscription="subscription"
+                    @edit="openEditModal"
+                    @delete="openDeleteModal"
+                />
+
+                <DeleteSubscriptionModal
+                    :show="showDeleteModal"
+                    :subscription="selectedSubscription"
+                    @delete="handleDeleted"
+                    @close="showDeleteModal = false"
+                />
             </div>
         </div>
     </AuthenticatedLayout>
